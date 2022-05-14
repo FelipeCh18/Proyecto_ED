de37ab4b23d5a70acb819caf1a1950b8d155f5fd
FelipeCh18 <fchaves@unal.edu.co>
2022-05-13 23:12:02 -0500

    clase edificio ya codificada y lista para implementar

---
+++ Edificio.py
@@ -0,0 +1,6 @@
+class Edificio():
+    def __init__(self, nombre_edificio=None, num_edificio=None, latitud=None, longitud=None):
+        self.nombre_edificio=nombre_edificio
+        self.num_edificio=num_edificio
+        self.latitud=latitud
+        self.longitud=longitud
