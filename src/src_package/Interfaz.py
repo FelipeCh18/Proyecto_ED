fcbdb14f9e9c503646a056ffb3b55dc1eac4c192
Homeroso <82355484+Homeroso@users.noreply.github.com>
2022-06-05 20:40:58 -0500

    Implementar BST y calculo de viaje con la interfaz

--- src/src_package/Interfaz.py
+++ src/src_package/Interfaz.py
@@ -1,9 +1,13 @@
 from tkinter import *
+from tkinter import messagebox
 import time
 from tkinter.ttk import Notebook
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import tkintermapview
+import unicodedata
+from prototipo import *
+from prototipo import edificios as info_ubicacion, Edificios_Nombre as info_nombre, Edificios_Numero as info_num
 
 raiz = Tk()
 raiz.title("MovilizateUN")
@@ -63,11 +67,36 @@ def crearViaje():
     desde=origen.get()
     hasta=destino.get()
     
-    #posicion y texto de los marcadores
-    marcador_origen.set_position(4.637574, -74.082677) 
-    marcador_origen.set_text(desde)
-    marcador_destino.set_position(4.638007, -74.084678)
-    marcador_destino.set_text(hasta)
+    desde=origen.get()
+    desde = unicodedata.normalize('NFKD', str(desde)).encode('ASCII', 'ignore').lower()
+    hasta=destino.get()
+    hasta = unicodedata.normalize('NFKD', str(hasta)).encode('ASCII', 'ignore').lower()
+    #comprobar si los datos ingresados existen
+    if (desde in info_nombre and hasta in info_nombre) or (desde in info_num and hasta in info_num):
+        
+        desde_ubicacion  = info_ubicacion[info_nombre.index(desde)]
+        
+        marcador_origen.set_position(desde_ubicacion.coordenadas[0], desde_ubicacion.coordenadas[1]) 
+        marcador_origen.set_text(desde.upper())
+
+        hasta_ubicacion = info_ubicacion[info_nombre.index(hasta)]
+        marcador_destino.set_position(hasta_ubicacion.coordenadas[0], hasta_ubicacion.coordenadas[1])
+        marcador_destino.set_text(hasta.upper())
+
+        #buscar la informacion en el arbol BST
+        ori=arbol_edificios.find(desde_ubicacion.num_edificio, arbol_edificios.root)
+        dest=arbol_edificios.find(hasta_ubicacion.num_edificio, arbol_edificios.root)
+
+        #calcular distancia entre puntos
+        viaje1 = Viaje(ori, dest)
+        viaje1.hacer_viaje()
+        historial_general.print_list()
+
+        #mostrar la distancia en pantalla
+            #distancia = Label(text = str(viaje1.hacer_viaje()), font="Candara, 20",bg='white', fg="black")
+            #distancia.place(x=100, y=220)
+    else:
+        print(messagebox.showinfo(message = 'Ingrese un edificio valido', title = 'Edificio no encontrado'))
     
     return desde,hasta
 
@@ -90,22 +119,51 @@ finViaje=Button(abajo, text="Finalizar Viaje", command = finalizarViaje, bg="#46
 finViaje.place(x=100, y=15)
 
 def abrirHist():
-    welcome.destroy()
-    inViaje.destroy()
-    finViaje.destroy()
-    historial.destroy()
-    volver.place(x=20, y=15)
+    global tam_barra, expanded
+    #abrir la barra lateral
+    if expanded is False:
+        tam_barra += 10 
+        rep = raiz.after(5,abrirHist) 
+        barra.config(width=tam_barra) 
+        if tam_barra >= max_barra:
+            expanded = True 
+            raiz.after_cancel(rep)
+            fill()
+    #cerrar la barra lateral
+    else:
+        tam_barra -= 10 
+        rep = raiz.after(5,abrirHist) 
+        barra.config(width=tam_barra)
+        if tam_barra <= min_barra: 
+            expanded = False 
+            raiz.after_cancel(rep)
+            fill()
     #añade los viajes y los muestra en pantalla
 
+#comprobar si la barra esta abierta
+def fill():
+    if expanded:
+        texto_viajes.config(text = 'Últimos viajes',image='',font=(0,21))
+    else:
+        texto_viajes.config(text = "")
+   
 #boton historial
 historial=Button(abajo, text="Historial", command=abrirHist, bg="#466b3f", fg="black")
 historial.place(x=230, y=15)
 
-def devolver():
-    #borra el historial
-    inViaje.place(x=20, y=15)
-    finViaje.place(x=100, y=500)
-    historial.place(x=230, y=15)
+#propiedades de la barra lateral
+min_barra = 10 
+max_barra = 200 
+tam_barra = 10 
+expanded = False 
+
+#crear la barra lateral para el historial
+raiz.update()
+barra = Frame(raiz,bg="#466b3f",width=10,height=raiz.winfo_height())
+barra.grid(row=0,column=0) 
+texto_viajes = Label(barra,bg="white",font="Candara 13", relief='flat')
+texto_viajes.grid(row=0,column=0,pady=10)
+barra.grid_propagate(False)
 
 #boton volver
 volver=Button(abajo, text="Volver", command="devolver", bg="#466b3f", fg="black")
